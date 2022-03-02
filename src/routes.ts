449613e7acaa0f089c791af8482e050faa469f8c
Binh Le <binhle.vn74@gmail.com>
2022-03-02 15:10:08 +0700

    Add calculate dest/source amount

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { calculateSwapAmountOut } from "./utils/amm/pool";
+import { calculateDestinationDataAmm, calculateSourceDataAmm } from "./utils/swap-amm";
 
 const routes = Router();
 
@@ -28,6 +29,55 @@ routes.post('/swapAmountOut', (req, res) => {
     }
     console.log(`---> Respond ${amountOutList.length} 'amount out' item(s).` );
     return res.json(amountOutList);
+});
+
+routes.post('/calculateDestinationsDataAmm', (req, res) => {
+    let requestBody = req.body;
+    let sourceAmounts = req.body.sourceAmounts;
+    console.log(`---> Calculate DESTINATION amount, source coin='${requestBody.sourceCoin}', destination coin='${requestBody.destinationCoin}'.` );
+
+    let destinationDataArguments: any = {
+        sourceCoin: requestBody.sourceCoin,
+        destinationCoin: requestBody.destinationCoin,
+        poolStates: requestBody.poolStates,
+        swapRoutes: requestBody.swapRoutes
+    };
+
+    let destinationDataList = [];
+    for (let sourceAmount of sourceAmounts) {
+        destinationDataArguments.sourceAmount = sourceAmount;
+        let destinationData = calculateDestinationDataAmm(destinationDataArguments);
+        destinationDataList.push({
+            sourceAmount: sourceAmount,
+            destinationAmount: destinationData.rawDestinationAmountAmm
+        });
+    }
+    console.log(`---> Respond ${destinationDataList.length} 'destination amount' item(s).` );
+    return res.json(destinationDataList);
+});
+
+routes.post('/calculateSourcesDataAmm', (req, res) => {
+    let requestBody = req.body;
+    let destinationAmounts = req.body.destinationAmounts;
+    console.log(`---> Calculate SOURCE amount, destination coin='${requestBody.destinationCoin}', source coin='${requestBody.sourceCoin}'.` );
+    let sourceDataArguments: any = {
+        sourceCoin: requestBody.sourceCoin,
+        destinationCoin: requestBody.destinationCoin,
+        poolStates: requestBody.poolStates,
+        swapRoutes: requestBody.swapRoutes
+    };
+
+    let sourceDataList = [];
+    for (let destinationAmount of destinationAmounts) {
+        sourceDataArguments.destinationAmount = destinationAmount;
+        let sourceData = calculateSourceDataAmm(sourceDataArguments);
+        sourceDataList.push({
+            destinationAmount: destinationAmount,
+            sourceAmount: sourceData.rawSourceAmountAmm
+        });
+    }
+    console.log(`---> Respond ${sourceDataList.length} 'source amount' item(s).` );
+    return res.json(sourceDataList);
 });
 
 export default routes;
